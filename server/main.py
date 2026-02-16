c4e436afb094b078002ab9b2a02c46d4dabb74bd
Cu80 <oliver.cubillos@gmx.com>
2026-02-16 19:27:59 +0000

    chore: backend entrypoint -> (connects to mongo and specifies how api handles the reqs)

--- server/main.py
+++ server/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
 
-app = FastAPI()
+load_dotenv(dotenv_path="../.env")
+
+app = FastAPI() # creating application instance
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # default port vite port
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# tells server to accept reqs from my frontend (otherwise it'll be blocked)
+
+
+# MongoDB connection
+mongo_client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
+db = mongo_client["graewatch"] # default db name for this which is graewatch
 
 @app.get("/api/health")
-def health():
-    return {"status": "ok"}
+async def health():
+    return {"status": "ok"} # health test
